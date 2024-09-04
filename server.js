26c1b5865856fafca9f707264be2e0a6faf5b825
NAVEEN15020189 <ramadevi15020@gmail.com>
2024-09-04 18:34:30 +0530

    aas

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://mernfrontendchat.onrender.com/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
